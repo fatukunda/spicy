520bb4abeab174164d36e463b3fb139395a8d505
Frank Atukunda <fatukunda@gmail.com>
2019-10-02 16:03:49 +0300

    ft(healthy eating steps)
     - Build the healthy-tips component.

---
+++ src/components/EatingTips/EatingTip.vue
@@ -0,0 +1,45 @@
+<template>
+  <div
+    class="eating-tip"
+    :style="{ backgroundColor: background, color: color }"
+  >
+    <div
+      class="tip"
+      :style="{ backgroundColor: idBackgroundColor, color: idColor }"
+    >
+      <h5 class="text-center">{{ id }}</h5>
+    </div>
+    <p class="tip-description">{{ tip }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EatingTip",
+  props: ["id", "tip", "color", "background", "idColor", "idBackgroundColor"]
+};
+</script>
+
+<style>
+.tip {
+  width: 5rem;
+  height: 5rem;
+  border-radius: 50%;
+  position: absolute;
+  top: -2rem;
+  left: -1rem;
+}
+.tip h5 {
+  line-height: 5rem;
+}
+.eating-tip {
+  position: relative;
+  height: 13rem;
+  border-radius: 0 2rem 0 2rem;
+}
+.tip-description {
+  font-size: 12px;
+  line-height: 1.5rem;
+  padding-top: 4rem;
+}
+</style>
---
+++ src/components/EatingTips/EatingTips.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="container-fluid eating-tips">
+    <div class="row intro">
+      <div class="col-md-12">
+        <small class="how-it-works">How it works</small>
+        <h5 class="font-weight-bold pt-2">
+          <span class="three-steps">3 Steps for</span>
+          <span class="healthy-eating">Healthy Eating</span>
+        </h5>
+      </div>
+      <div class="col-md-12">
+        <div class="row tips">
+          <div class="col-md-4 single-tip" v-for="tip in tips" :key="tip.id">
+            <EatingTip
+              :id="tip.id"
+              :tip="tip.tip"
+              :background="tip.background"
+              :color="tip.color"
+              :idColor="tip.idColor"
+              :idBackgroundColor="tip.idBackgroundColor"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import EatingTip from "./EatingTip";
+export default {
+  name: "EatingTips",
+  components: {
+    EatingTip
+  },
+  data() {
+    return {
+      tips: [
+        {
+          id: "01",
+          tip:
+            "available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable",
+          background: "#FDBE31",
+          color: "#ffffff",
+          idBackgroundColor: "#5A2F27",
+          idColor: "#ffffff"
+        },
+        {
+          id: "02",
+          tip:
+            "available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable",
+          background: "#EAE9E4",
+          color: "##190707",
+          idBackgroundColor: "#190707",
+          idColor: "#ffffff"
+        },
+        {
+          id: "03",
+          tip:
+            "available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable",
+          background: "#FDBE31",
+          color: "#ffffff",
+          idBackgroundColor: "#5A2F27",
+          idColor: "#ffffff"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped>
+.eating-tips {
+  margin-top: 6rem;
+  margin-bottom: 4rem;
+}
+.intro {
+  text-align: center;
+  width: 60%;
+  margin: 0 auto;
+}
+.three-steps {
+  color: #fdbe31;
+}
+.healthy-eating {
+  color: #5a2f27;
+  padding-left: 0.3rem;
+}
+.how-it-works {
+  color: #b3b1a6;
+}
+.tips {
+  margin-top: 4rem;
+}
+
+@media screen and (max-width: 600px) {
+  .intro {
+    width: 100%;
+    margin-top: 80rem;
+  }
+  .single-tip {
+    margin-top: 0.5rem;
+  }
+}
+</style>
