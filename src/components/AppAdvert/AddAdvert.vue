7ee6d9ad75487fceae49fd1e30335e4c99b6a7c2
Frank Atukunda <fatukunda@gmail.com>
2019-10-03 15:06:22 +0300

    ft(Advert and footer): Add advert and footer components
     - Build advert and footer components

---
+++ src/components/AppAdvert/AddAdvert.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="container main">
+    <div class="row main-info">
+      <div class="col-md-12 text-center">
+        <h5>Free Multipurpose</h5>
+        <h5>Responsive Landing Page 2019</h5>
+        <p class="light-yellow">
+          <small>Modern lighting fast & easily customizable</small>
+        </p>
+        <button class="mt-4 quote-button">Get a quote</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppAdvert"
+};
+</script>
+
+<style scoped>
+.main {
+  height: 20rem;
+}
+.main-info {
+  margin-top: 6rem;
+}
+.light-yellow {
+  color: #eda911;
+  font-size: 0.8rem;
+}
+.quote-button {
+  background-color: #eda911;
+  width: 10rem;
+  height: 2rem;
+  border: 0;
+  border-radius: 4rem;
+  color: #ffffff;
+  font-size: 0.8rem;
+}
+@media screen and (max-width: 600px) {
+  .main-info {
+    margin-top: 22rem;
+  }
+  .main {
+    height: 15rem;
+  }
+}
+</style>
---
+++ src/components/Footer/AppFooter.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container-fluid footer">
+    <div class="row">
+      <div class="col-md-12">
+        <p class="text-center pt-4">
+          <span class="pr-2">Designed by Free html templates.</span>
+          <span class="pr-2">Developed by</span>
+          <span class="portfolio"
+            >Frank Atukunda - JavaScript Engineer @Andela</span
+          >
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppFooter"
+};
+</script>
+
+<style scoped>
+.footer {
+  height: 5rem;
+  background-color: #582e26;
+  color: #ffffff;
+  font-size: 12px;
+}
+.portfolio {
+  color: #eda911;
+}
+</style>
